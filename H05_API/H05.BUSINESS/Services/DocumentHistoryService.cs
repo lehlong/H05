a0d4df63798e5b83eec4d3df1bb14bb779b5fa57
Hải Long <hlonn.it@gmail.com>
2025-05-19 19:16:24 +0700

    Update DocumentHistoryService.cs

--- H05_API/H05.BUSINESS/Services/DocumentHistoryService.cs
+++ H05_API/H05.BUSINESS/Services/DocumentHistoryService.cs
@@ -1,1 +1,35 @@
- 
+using AutoMapper;
+using Common;
+using H05.BUSINESS.Common;
+using H05.BUSINESS.Dtos;
+using H05.CORE;
+using H05.CORE.Entities;
+
+namespace H05.BUSINESS.Services
+{
+    public interface IDocumentHistoryService : IGenericService<TblDocumentHistory, DocumentHistoryDto>
+    {
+    }
+    
+    public class DocumentHistoryService : GenericService<TblDocumentHistory, DocumentHistoryDto>, IDocumentHistoryService
+    {
+        public DocumentHistoryService(AppDbContext dbContext, IMapper mapper) : base(dbContext, mapper)
+        {
+        }
+        
+        public override async Task<PagedResponseDto> Search(BaseFilter filter)
+        {
+            try
+            {
+                var query = _dbContext.TblDocumentHistory.AsQueryable();
+                return await Paging(query, filter);
+            }
+            catch (Exception ex)
+            {
+                Status = false;
+                Exception = ex;
+                return null;
+            }
+        }
+    }
+} 
